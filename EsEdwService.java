b940dbbee70cd088e41c4c86bc8657357bbb137b
Omkar Dhaybar <95211202+omkardhaybar@users.noreply.github.com>
2021-12-23 01:25:56 +0530

    Update EsEdwService.java

--- EsEdwService.java
+++ EsEdwService.java
@@ -1,13 +1,13 @@
-package com.om.springbootall.service;
-//package com.unfi.esedw.service
-import com.om.springbootall.dtoo.CustomerDto;
-import com.om.springbootall.dtoo.CustomerSalesDataResponse;
-import com.om.springbootall.dtoo.CustumerSalesDataFromDWResponse;
-import com.om.springbootall.dtoo.ResponseDto;
-import com.om.springbootall.entity.CustomerSale;
-import com.om.springbootall.mapper.CustomerMapper;
-import com.om.springbootall.model.RequestDto;
-import com.om.springbootall.repository.CustomerRepository;
+package com.unfi.esedw.service;
+
+import com.unfi.esedw.dto.CustomerDto;
+import com.unfi.esedw.dto.CustomerSalesDataResponse;
+import com.unfi.esedw.dto.CustumerSalesDataFromDWResponse;
+import com.unfi.esedw.dto.ResponseDto;
+import com.unfi.esedw.entity.CustomerSale;
+import com.unfi.esedw.mapper.CustomerMapper;
+import com.unfi.esedw.dto.RequestDto;
+import com.unfi.esedw.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Arrays;
@@ -26,21 +26,21 @@ public class EsEdwService{
     @Autowired
     CustomerSalesDataResponse customerSalesdataResponse;
 
-    public ResponseDto getSalesDataDW(RequestDto requestDto) {
+    public ResponseDto getSalesDataDW(RequestDtoo requestDto) {
 
-        List<String> unitId=Arrays.asList(requestDto.getCsdRequest().getUnit_id().split(","));
-        List<String> upc=Arrays.asList(requestDto.getCsdRequest().getUpc().split(","));
-        String salesBeginDate=requestDto.getCsdRequest().getBeginDate();
-        String salesEndDate=requestDto.getCsdRequest().getEndDate();
+        List<String> unitId = Arrays.asList(requestDto.getGetCustumerSaleData().getCustomerSalesDataRequest().getUnitId().split(","));
+        List<String> upc = Arrays.asList(requestDto.getGetCustumerSaleData().getCustomerSalesDataRequest().getUpc().split(","));
+        String salesBeginDate = requestDto.getGetCustumerSaleData().getCustomerSalesDataRequest().getSalesBeginDate();
+        String salesEndDate = requestDto.getGetCustumerSaleData().getCustomerSalesDataRequest().getSalesEndDate();
 
-        List<CustomerSale> customerSale= customerRepository.getCustomerSale(unitId,upc,salesBeginDate,salesEndDate);
+        List<CustomerSale> customerSale = customerRepository.getCustomerSale(unitId,upc,salesBeginDate,salesEndDate);
 
-        List<CustomerDto> customerDto=customerMapper.map(customerSale);
+        List<CustomerDto> customerDto = customerMapper.map(customerSale);
         //setting response from db (list of customer sales data), inner json loop(Record)
         customerSalesdataResponse.setRecords(customerDto);
         //setting system, correlarion id
-        custumerSalesDataFromDWResponse.setSystem(requestDto.getSystem());
-        custumerSalesDataFromDWResponse.setCorrelationId(requestDto.getCorrelationId());
+        custumerSalesDataFromDWResponse.setSystem(requestDto.getGetCustumerSaleData().getSystem());
+        custumerSalesDataFromDWResponse.setCorrelationId(requestDto.getGetCustumerSaleData().getCorrelationId());
         custumerSalesDataFromDWResponse.setCustomerSalesdataResponse(customerSalesdataResponse);
         //setting whole json inner loops in response
         responseDto.setCustumerSalesDataFromDWResponse(custumerSalesDataFromDWResponse);
